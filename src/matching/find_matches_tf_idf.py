5251d0eea91eceb479c4132faf55b63fe513edcc
jlevine18 <jlevine4@illinois.edu>
2022-10-04 11:28:29 -0500

    Add queries modifications

--- src/matching/find_matches_tf_idf.py
+++ src/matching/find_matches_tf_idf.py
@@ -61,3 +61,26 @@ def main_loop(di, block_size, threshold, fileloc):
     os.chdir(di)
     files = os.listdir()
     tf_idf_dots_blocked(files, block_size, threshold = threshold, fileloc = fileloc)
+    
+def tf_idf_dots_blocked_queries(corpus, block_size, threshold = .95, fileloc="matches_queries.txt"):
+    matches = {}
+    for i in range(0, len(corpus), block_size):
+            for j in range(0, len(corpus), block_size):
+                block_match = {}
+                mtx = tf_idf_dots_comparitive(corpus[i:min(i+block_size, len(corpus))], corpus[j:min(j+block_size, len(corpus))])
+                coordinates = np.where(mtx > threshold)
+                for k in range(len(coordinates[0])):
+                    x = coordinates[0][k]
+                    y = coordinates[1][k]
+                    if i+x == j+y:
+                        continue
+                    block_match[(i+x,j+y)] = mtx[x][y]
+                with open(fileloc, "a+") as f:
+                    for m in block_match:
+                        matches[m] = block_match[m]
+                        v,u = m
+                        f.write("(" + str(v) +", " + str(u) + "): " + str(matches[(v,u)]) + "\n")
+
+def main_loop_queries(di, block_size, threshold, fileloc):
+    texts = list(pd.read_csv(di + "/queries_train.tsv", delimiter = "\t", names=["n", "text"])["text"]) + list(pd.read_csv(di + "/queries_val.tsv", delimiter = "\t", names=["n", "text"])["text"]) + list(pd.read_csv(di + "/queries_test.tsv", delimiter = "\t", names=["n", "text"])["text"])
+    tf_idf_dots_blocked_queries(texts, block_size, threshold = threshold, fileloc = fileloc)
