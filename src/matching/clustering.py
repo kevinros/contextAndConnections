3b60de60d19c26450250017d4037472390ccef30
jlevine18 <jacoblevine18@gmail.com>
2022-10-04 11:38:55 -0500

    fix formatting

--- src/matching/clustering.py
+++ src/matching/clustering.py
@@ -1,68 +1,95 @@
 import re
 
+
 class disjoint_set:
     def __init__(self, n):
         self.vals = [-1 for i in range(n)]
+
     def query(self, n):
         if self.vals[n] < 0:
             return n
         q = self.query(self.vals[n])
         self.vals[n] = q
         return q
+
     def merge(self, n1, n2):
         q1 = self.query(n1)
         q2 = self.query(n2)
-        if (q1 == q2):
+        if q1 == q2:
             return
-        if (self.vals[q1] < self.vals[q2]):
-            #q1 is larger than q2
+        if self.vals[q1] < self.vals[q2]:
+            # q1 is larger than q2
             self.vals[q1] += self.vals[q2]
             self.vals[q2] = q1
         else:
             self.vals[q2] += self.vals[q1]
             self.vals[q1] = q2
+
     def find_set_size(self, n):
         return self.vals[self.query(n)]
+
     def count_nonzero(self):
         count = 0
         for i in self.vals:
             if i < 0:
                 count += 1
         return count
+
     def largest_set(self):
         return -min(self.vals)
-      
+
+
 parser = re.compile("\((\d+), (\d+)\): (\d\.\d*)")
+
+
 def cluster(matches_file, threshold, n_files):
-  ds = disjoint_set(n_files)
-  with open(matches_file, "r") as f:
-    while True:
-        line = f.readline()
-        if line is None or line == "":
-            break
-        i1, i2, val = parser.match(line).groups()
-        i1 = int(i1)
-        i2 = int(i2)
-        val = float(val)
-        if val > threshold:
-            ds.merge(i1, i2)
-  return ds
+    ds = disjoint_set(n_files)
+    with open(matches_file, "r") as f:
+        while True:
+            line = f.readline()
+            if line is None or line == "":
+                break
+            i1, i2, val = parser.match(line).groups()
+            i1 = int(i1)
+            i2 = int(i2)
+            val = float(val)
+            if val > threshold:
+                ds.merge(i1, i2)
+    return ds
+
 
 def write_out(ds, files):
-  with open("clusters.csv", "w+") as f:
-      f.write("file, cluster\n")
-      for i, im in enumerate(ds.vals):
-          if im < 0:
-              f.write(files[i] + ", " + files[i] +"\n")
-          else:
-              f.write(files[i] + ", " + files[ds.query(im)] +"\n")
+    with open("clusters.csv", "w+") as f:
+        f.write("file, cluster\n")
+        for i, im in enumerate(ds.vals):
+            if im < 0:
+                f.write(files[i] + ", " + files[i] + "\n")
+            else:
+                f.write(files[i] + ", " + files[ds.query(im)] + "\n")
+
 
 def write_out_queries(ds, di):
-    indices = list(pd.read_csv(di + "/queries_train.tsv", delimiter = "\t", names=["n", "text"])["n"]) + list(pd.read_csv(di + "/queries_val.tsv", delimiter = "\t", names=["n", "text"])["n"]) + list(pd.read_csv(di + "/queries_test.tsv", delimiter = "\t", names=["n", "text"])["n"])
+    indices = (
+        list(
+            pd.read_csv(di + "/queries_train.tsv", delimiter="\t", names=["n", "text"])[
+                "n"
+            ]
+        )
+        + list(
+            pd.read_csv(di + "/queries_val.tsv", delimiter="\t", names=["n", "text"])[
+                "n"
+            ]
+        )
+        + list(
+            pd.read_csv(di + "/queries_test.tsv", delimiter="\t", names=["n", "text"])[
+                "n"
+            ]
+        )
+    )
     with open("clusters_queries.csv", "w+") as f:
         f.write("global index, cluster\n")
-          for i, im in enumerate(ds.vals):
-              if im < 0:
-                  f.write(indices[i] + ", " + indices[i] +"\n")
-              else:
-                  f.write(indices[i] + ", " + indices[ds.query(im)] +"\n")
+        for i, im in enumerate(ds.vals):
+            if im < 0:
+                f.write(indices[i] + ", " + indices[i] + "\n")
+            else:
+                f.write(indices[i] + ", " + indices[ds.query(im)] + "\n")
--- src/matching/find_matches_tf_idf.py
+++ src/matching/find_matches_tf_idf.py
@@ -2,18 +2,20 @@ import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 
+
 def extract_content(files, start, stop):
     corpus = []
-    if (start >= len(files)):
+    if start >= len(files):
         start = len(files) - 1
-    if (stop >= len(files)):
+    if stop >= len(files):
         stop = len(files) - 1
     for i in range(start, stop):
         with open(files[i], "r") as f:
             entry = json.loads(f.read())
             corpus.append(entry["contents"])
-            
+
     return corpus
+
 
 def tf_idf_dots_(corpus):
     vectorizer = TfidfVectorizer()
@@ -21,14 +23,16 @@ def tf_idf_dots_(corpus):
     out = X @ X.T
     return out
 
+
 def tf_idf_dots_comparitive(corpus1, corpus2):
-    #only does upper triangle
+    # only does upper triangle
     vectorizer = TfidfVectorizer()
     X = vectorizer.fit_transform(corpus1 + corpus2)
-    out = np.array((X[0:len(corpus1)] @ X[len(corpus1):].T).todense())
+    out = np.array((X[0 : len(corpus1)] @ X[len(corpus1) :].T).todense())
     return out
 
-def tf_idf_dots_block(block_start_1, block_start_2, block_size, threshold = .95):
+
+def tf_idf_dots_block(block_start_1, block_start_2, block_size, threshold=0.95):
     matches = {}
     corpus_1 = extract_content(files, block_start_1, block_size + block_start_1)
     corpus_2 = extract_content(files, block_start_2, block_size + block_start_2)
@@ -37,13 +41,14 @@ def tf_idf_dots_block(block_start_1, block_start_2, block_size, threshold = .95)
     for k in range(len(coordinates[0])):
         x = coordinates[0][k]
         y = coordinates[1][k]
-        if block_start_1+x == block_start_2+y:
+        if block_start_1 + x == block_start_2 + y:
             continue
-        matches[(block_start_1+x,block_start_2+y)] = mtx[x][y]
-        print((block_start_1+x,block_start_2+y))
+        matches[(block_start_1 + x, block_start_2 + y)] = mtx[x][y]
+        print((block_start_1 + x, block_start_2 + y))
     return matches
 
-def tf_idf_dots_blocked(files, block_size, threshold = .95, fileloc="../matches.txt"):
+
+def tf_idf_dots_blocked(files, block_size, threshold=0.95, fileloc="../matches.txt"):
     matches = {}
     for i in range(0, len(files), block_size):
         for j in range(0, len(files), block_size):
@@ -52,35 +57,75 @@ def tf_idf_dots_blocked(files, block_size, threshold = .95, fileloc="../matches.
                 with open(fileloc, "a+") as f:
                     for m in block_match:
                         matches[m] = block_match[m]
-                        v,u = m
-                        f.write("(" + str(v) +", " + str(u) + "): " + str(matches[(v,u)]) + "\n")
-            if(j//block_size % 10 == 0):
-                print((i,j))
+                        v, u = m
+                        f.write(
+                            "("
+                            + str(v)
+                            + ", "
+                            + str(u)
+                            + "): "
+                            + str(matches[(v, u)])
+                            + "\n"
+                        )
+            if j // block_size % 10 == 0:
+                print((i, j))
+
 
 def main_loop(di, block_size, threshold, fileloc):
     os.chdir(di)
     files = os.listdir()
-    tf_idf_dots_blocked(files, block_size, threshold = threshold, fileloc = fileloc)
-    
-def tf_idf_dots_blocked_queries(corpus, block_size, threshold = .95, fileloc="matches_queries.txt"):
+    tf_idf_dots_blocked(files, block_size, threshold=threshold, fileloc=fileloc)
+
+
+def tf_idf_dots_blocked_queries(
+    corpus, block_size, threshold=0.95, fileloc="matches_queries.txt"
+):
     matches = {}
     for i in range(0, len(corpus), block_size):
-            for j in range(0, len(corpus), block_size):
-                block_match = {}
-                mtx = tf_idf_dots_comparitive(corpus[i:min(i+block_size, len(corpus))], corpus[j:min(j+block_size, len(corpus))])
-                coordinates = np.where(mtx > threshold)
-                for k in range(len(coordinates[0])):
-                    x = coordinates[0][k]
-                    y = coordinates[1][k]
-                    if i+x == j+y:
-                        continue
-                    block_match[(i+x,j+y)] = mtx[x][y]
-                with open(fileloc, "a+") as f:
-                    for m in block_match:
-                        matches[m] = block_match[m]
-                        v,u = m
-                        f.write("(" + str(v) +", " + str(u) + "): " + str(matches[(v,u)]) + "\n")
+        for j in range(0, len(corpus), block_size):
+            block_match = {}
+            mtx = tf_idf_dots_comparitive(
+                corpus[i : min(i + block_size, len(corpus))],
+                corpus[j : min(j + block_size, len(corpus))],
+            )
+            coordinates = np.where(mtx > threshold)
+            for k in range(len(coordinates[0])):
+                x = coordinates[0][k]
+                y = coordinates[1][k]
+                if i + x == j + y:
+                    continue
+                block_match[(i + x, j + y)] = mtx[x][y]
+            with open(fileloc, "a+") as f:
+                for m in block_match:
+                    matches[m] = block_match[m]
+                    v, u = m
+                    f.write(
+                        "("
+                        + str(v)
+                        + ", "
+                        + str(u)
+                        + "): "
+                        + str(matches[(v, u)])
+                        + "\n"
+                    )
+
 
 def main_loop_queries(di, block_size, threshold, fileloc):
-    texts = list(pd.read_csv(di + "/queries_train.tsv", delimiter = "\t", names=["n", "text"])["text"]) + list(pd.read_csv(di + "/queries_val.tsv", delimiter = "\t", names=["n", "text"])["text"]) + list(pd.read_csv(di + "/queries_test.tsv", delimiter = "\t", names=["n", "text"])["text"])
-    tf_idf_dots_blocked_queries(texts, block_size, threshold = threshold, fileloc = fileloc)
+    texts = (
+        list(
+            pd.read_csv(di + "/queries_train.tsv", delimiter="\t", names=["n", "text"])[
+                "text"
+            ]
+        )
+        + list(
+            pd.read_csv(di + "/queries_val.tsv", delimiter="\t", names=["n", "text"])[
+                "text"
+            ]
+        )
+        + list(
+            pd.read_csv(di + "/queries_test.tsv", delimiter="\t", names=["n", "text"])[
+                "text"
+            ]
+        )
+    )
+    tf_idf_dots_blocked_queries(texts, block_size, threshold=threshold, fileloc=fileloc)
